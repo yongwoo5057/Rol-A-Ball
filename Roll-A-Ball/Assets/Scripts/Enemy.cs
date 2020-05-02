49aff1daff4d3b32aa6b17b8f9d815806ae19615
unknown <yongwoo5057>
2020-05-02 12:17:56 +0900

    First Comment
    Added Unity project

---
+++ Roll-A-Ball/Assets/Scripts/Enemy.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int state;
+    public float speed = 0.06f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StateCoroutine());
+        StartCoroutine(MakeRandomNumber());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case 1:
+                Upstate();
+                break;
+            case 2:
+                Downstate();
+                break;
+        }
+    }
+
+    IEnumerator StateCoroutine()
+    {
+        while (true)
+        {
+            state = 1;
+            yield return new WaitForSeconds(1);
+            state = 2;
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    IEnumerator MakeRandomNumber()
+    {
+        while (true)
+        {
+            int randomNum = Random.Range(0, 6);
+
+            Debug.Log(randomNum);
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    void Upstate()
+    {
+        
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+
+    void Downstate()
+    {
+       
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+    void OnTriggerEnter(Collider other)
+
+    {
+
+        Destroy(other.gameObject);
+
+    }
+}
---
+++ Roll-A-Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text CountText;
+    public Text WinText;
+
+    private Rigidbody rb;
+    private int Count;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Count = 0;
+        SetCountText();
+        WinText.text = "";
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce (movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive (false);
+            Count = Count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        CountText.text = "Count: " + Count.ToString();
+        if(Count >= 8)
+        {
+            WinText.text = "You Win!";
+        }
+    }
+}
+
+//Destroy(other.gameObject);
+//if(other.gameObject.CompareTag("Player"))
+//      gameObject.SetActive (false);
